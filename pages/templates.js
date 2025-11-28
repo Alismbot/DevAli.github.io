aa9942088b0a9f32065b4bb347eb02b1cc3c2ce0
Alismbot <iconemoment1@gmail.com>
2025-11-28 12:19:11 +0100

    Update templates.js

--- pages/templates.js
+++ pages/templates.js
@@ -3,10 +3,5 @@ window.APPS = [
         page: "Gbwa.html",
         image: "assets/Down/mmx8.png",
         desc: "اضغط على الصورة للانتقال مباشرة إلى صفحة تحميل GBWhatsApp Pro v23.96"
-    },
-    {
-        page: "Gbwa.html",
-        image: "assets/Down/mmx7.png",
-        desc: "اضغط على الصورة للانتقال مباشرة إلى صفحة تحميل GBWhatsApp Pro v23.96"
     }
 ];
