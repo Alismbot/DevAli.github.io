f89e413f1234332abb1877e94b40f2ce7f806be2
Alismbot <iconemoment1@gmail.com>
2025-11-30 13:04:11 +0100

    Add series list and styling to series.html

--- pages/templates.js
+++
@@ -1,7 +0,0 @@
-window.APPS = [
-    {
-        page: "Gbwa.html",
-        image: "assets/Down/mmx8.png",
-        desc: "اضغط على الصورة للانتقال مباشرة إلى صفحة تحميل GBWhatsApp Pro v23.96"
-    }
-];
